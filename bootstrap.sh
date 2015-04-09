62e123202e71800e330b66f9679090ad79065fc9
omri <omri.ey@walkme.com>
2015-04-09 20:32:03 +0300

    newline?

--- bootstrap.sh
+++ bootstrap.sh
@@ -26,3 +26,4 @@ sudo apt-get install -y python-software-properties
 sudo add-apt-repository -y ppa:cassou/emacs
 sudo apt-get -qq update
 sudo apt-get install -y emacs24-nox emacs24-el emacs24-common-non-dfsg
+
