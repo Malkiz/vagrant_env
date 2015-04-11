d25d49864b556024542f28d7df3777cc26289f98
omri <omri.ey@walkme.com>
2015-04-11 21:04:54 +0300

    bootstrap nodejs, npm, nvm

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,29 +1,34 @@
 #!/usr/bin/env bash
 
+# Installing dependencies:
 sudo apt-get update
-sudo apt-get install -y git-core
 sudo apt-get install -y curl
-
-# install nvm v0.24.1
+sudo apt-get install -y git git-core
+sudo apt-get install -y gcc g++ autoconf automake libtool
+sudo apt-get install -y --fix-missing python-software-properties python make
 sudo apt-get install -y build-essential libssl-dev
-curl https://raw.githubusercontent.com/creationix/nvm/v0.24.1/install.sh | bash
-
-# Load nvm and install latest production node
-source $HOME/.nvm/nvm.sh
-nvm install v0.12.2
-nvm use v0.12.2
-
-# Install jshint to allow checking of JS code within emacs
-# http://jshint.com/
-npm install -g jshint
+sudo apt-get install -y software-properties-common
 
 # Install rlwrap to provide libreadline features with node
 # See: http://nodejs.org/api/repl.html#repl_repl
 sudo apt-get install -y rlwrap
 
+# Installing last node and npm version
+sudo apt-add-repository -y ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get install -y nodejs
+
+# Install nvm: node-version manager
+# Link: https://www.npmjs.org/package/nvm
+sudo npm install -g nvm
+sudo export PATH=./node_modules/.bin:$PATH
+
+# Install jshint to allow checking of JS code within emacs
+# http://jshint.com/
+sudo npm install -g jshint
+
 # Install emacs24
 # https://launchpad.net/~cassou/+archive/emacs
-sudo apt-get install -y --fix-missing python-software-properties
 sudo add-apt-repository -y ppa:cassou/emacs
 sudo apt-get -qq update
 sudo apt-get install -y emacs24-nox emacs24-el emacs24-common-non-dfsg
